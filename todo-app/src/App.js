57c9a171c2b9e3ae039dba7e9338c93bc4e20feb
ranjitm10 <64469138+ranjitm10@users.noreply.github.com>
2021-04-29 23:40:48 +0530

    started with Todo-App

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./mycomponent/header";
+import Footer from "./mycomponent/footer";
+import Todo from "./mycomponent/todo";
+import Todos from "./mycomponent/todos";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Header/>
+     <Footer/>
+     <Todo/>
+     <Todos/>
+    </>
   );
 }
 
